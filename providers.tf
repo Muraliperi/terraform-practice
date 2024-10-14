93dc7e09af32900e75183254af421838232ccc2c
Muraliperi <58391262+Muraliperi@users.noreply.github.com>
2024-10-14 19:09:55 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,1 +1,7 @@
 
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+  access_key = "xxyyzz"   # Not recommended to update access key and secret key here
+  secret_key = "aabbcc" # Not recommended to update access key and secret key here
+}
