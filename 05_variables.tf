867e86cf74b0df3c0e02839f72499c7f121b9023
Muraliperi <58391262+Muraliperi@users.noreply.github.com>
2024-10-16 09:30:02 +0530

    Update 05_variables.tf

--- 05_variables.tf
+++ 05_variables.tf
@@ -28,3 +28,45 @@ variable "public_subnets" {
     "public_subnet_3" = 3
   }
 }
+
+variable "variables_sub_cidr" {
+  description = "CIDR block for the variable subnet"
+  type = string
+  default = "10.0.251.0/24"
+}
+
+variable "variables_availability_zone" {
+  description = "Availability Zone used Variable Subnet"
+  type = string
+  default = "us-east-1a"
+}
+
+variable "variable_auto_launch_pub_ip" {
+  description = "Auto launch Public IP for Variable Subnet"
+  type = bool
+  default = true
+}
+
+variable "variables_dev_environment" {
+  description = "Variable name for Dev Environment"
+  type = string
+  default = "dev"
+}
+
+variable "variables_prod_environment" {
+  description = "Variable name for Prod Environment"
+  type = string
+  default = "prod"
+}
+
+variable "variables_stage_environment" {
+  description = "Variable name for Stage Environment"
+  type = string
+  default = "stage"
+}
+
+variable "variables_test_environment" {
+  description = "Variable name for Test Environment"
+  type = string
+  default = "test"
+}
